ffa1d26ca24ac05273d7a01cdda7915a070e9d48
Dante Escame <70451220+dante-escame@users.noreply.github.com>
2021-09-15 12:04:58 -0300

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using API.Controllers;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+  public class ActivitiesController : BaseApiController
+  {
+    private readonly DataContext _context; // DataContext possui o atributo Activities(do tipo DbSet)
+
+    public ActivitiesController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+      return await _context.Activities.ToListAsync(); // retorna atividades do banco
+    }
+
+    [HttpGet("{id}")] // activies/id
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+      return await _context.Activities.FindAsync(id);
+    }
+  }
+}
